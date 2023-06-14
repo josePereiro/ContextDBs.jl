c3dbe868f152b8c1042ca2ba9464efd136a01c00
josePereiro <jose.pereiro.personal@gmail.com>
2023-06-14 05:13:02 -0400

    Add some tests, passing

--- test/runtests.jl
+++ test/runtests.jl
@@ -5,38 +5,62 @@ using Test
 @testset "ContextDBs.jl" begin
 
     ## ------------------------------------------------------------------
-    # CacheRef
-    @emptycontextdb!
-    @tempcontext ["CACHE_REF"] let
+    # @tempcontext
+    let
+        let
+            @newcontextdb!
+            @test length(contextdb()) == 0
     
-        Random.seed!(123)
-        
-        cacherefs_dir!(tempdir())
+            _val0 = rand(10,10)
+            @tempcontext ["subcontext"] begin
+                @test @isdefined _val0
+                @stage! _val1 = _val0
+            end
+            @test @isdefined _val1
+            @test length(contextdb()) == 1
+            _val2 = query(["ROOT"])["_val1"]
+            @test _val1 === _val0
+            @test _val2 === _val0
+        end
+        # Test it is contained by outer scope
+        @test !@isdefined _val1
+    end
 
-        # stage ref
-        _dat0 = rand(10,10)
-        @stage! _ref0 = CacheRef(_dat0)
+    ## ------------------------------------------------------------------
+    # CacheRef
+    let
+        @newcontextdb!
+        @tempcontext ["CACHE_REF"] let
         
-        # I can use ref from the db 
-        # (this is just an usage example)
-        _ref1 = context("_ref0")
+            Random.seed!(123)
+            
+            cacherefs_dir!(tempdir())
 
-        @test _ref0 === _ref1
-        
-        # this load a new copy
-        _dat1 = _ref0[]
-        _dat2 = _ref1[]
-        
-        try
-            @test isfile(_ref0)
-            @test isfile(_ref1)
-            @test all(_dat0 .== _dat1)
-            @test all(_dat1 .== _dat2)
-            @test _dat0 !== _dat1
-            @test _dat1 !== _dat2
-        finally    
-            rm(_ref0; force = true)
-            rm(_ref1; force = true)
+            # stage ref
+            _dat0 = rand(10,10)
+            @stage! _ref0 = CacheRef(_dat0)
+            
+            # I can use ref from the db 
+            # (this is just an usage example)
+            _ref1 = context("_ref0")
+
+            @test _ref0 === _ref1
+            
+            # this load a new copy
+            _dat1 = _ref0[]
+            _dat2 = _ref1[]
+            
+            try
+                @test isfile(_ref0)
+                @test isfile(_ref1)
+                @test all(_dat0 .== _dat1)
+                @test all(_dat1 .== _dat2)
+                @test _dat0 !== _dat1
+                @test _dat1 !== _dat2
+            finally    
+                rm(_ref0; force = true)
+                rm(_ref1; force = true)
+            end
         end
     end
 
